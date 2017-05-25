d3d03cf8a86f9cd30a0025ec8065a32c472c2cd4
jiaoweimng215 <jiaoweimng215@163.com>
2017-05-25 17:28:24 +0800

    I am Joe

---
+++ ex1.py
@@ -0,0 +1,10 @@
+
+#-*-coding:utf-8-*-
+
+print "你 好  World!"
+print "Hello Again"
+print "I like typint this."
+print "This is fun"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+#print 'I "said" do not touch this'
