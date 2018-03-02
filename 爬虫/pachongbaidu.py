d4ff60fc6c5168d7cc2aa4645dcfc6af12556a22
jiaoweimng215 <jiaoweimng215@163.com>
2018-03-02 17:32:43 +0800

    a

--- 爬虫/pachongbaidu.py
+++ 爬虫/pachongbaidu.py
@@ -3,6 +3,19 @@
 import urllib
 from bs4 import BeautifulSoup
 
+html=urllib.urlopen("http://www.21ic.com/")
+
+response = BeautifulSoup(html.read(),'html.parser')
+
+text_list = response.find_all("a","target")
+
+for text in text_list:
+    print text.get_text()
+
+html.close()
+
+
+'''
 html=urllib.urlopen("http://www.baidu.com")
 
 response = BeautifulSoup(html.read(),'html.parser')
@@ -13,3 +26,4 @@ for text in text_list:
     print text.get_text()
 
 html.close()
+'''
